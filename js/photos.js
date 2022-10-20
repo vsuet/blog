2aa2c0601209b7fa2120a7052719a9b37a86708d
Angelina <koao2002@mail.ru>
2022-10-20 17:05:50 +0300

    работа доделана

--- js/photos.js
+++ js/photos.js
@@ -6,47 +6,22 @@ let photos = [];
 function Photos() {
     choiceList.classList.add('show');
 
-   /* addPhotos.classList.add('show');
+    addPhotos.classList.add('show');
 
     fetch("https://jsonplaceholder.typicode.com/photos")
     .then(response => response.json())
     .then(addPhotosArr => addPhotosArr.forEach(photo => {
-        addPhotos.innerHTML += 
-        `<div class="js-photos"id="js-add-photos">
-            <div class="together">
-                <img src="${photo.thumbnailUrl}" alt="${photo.title}" class="url">
-                <p class="title">${photo.title}</p>
-            </div>
-            <p class="id">id:${photo.id}</p>
-            <hr>
-        </div>`
-        photos.push(photo);
-}))*/
-};
-
-
-
-/*const btnOpenPhotos = document.querySelector('.js-btn-photos'),
-choiceList = document.querySelector('.choice-list-h2');
-
-const tablePhotos = document.querySelector('.table-photos');
-
-btnOpenPhotos.addEventListener('click', function(){
-    choiceList.classList.add('show');
-
-    tablePhotos.classList.add('show');
-  
-    let elPhotos = document.querySelector('.js-photos');
-  
-    for (let index = 0; index < photos.length; index++) {
-        elPhotos.innerHTML += 
+        if(photo.albumId === 1){
+            addPhotos.innerHTML += 
             `<div class="js-photos"id="js-add-photos">
                 <div class="together">
-                    <img src="${photos[index].thumbnailUrl}" alt="" class="url">
-                    <p class="title">${photos[index].title}</p>
+                    <img src="${photo.thumbnailUrl}" alt="${photo.title}" class="url">
+                    <p class="title">${photo.title}</p>
                 </div>
-                <p class="id">id:${photos[index].id}</p>
+                <p class="id">id:${photo.id}</p>
                 <hr>
             </div>`
-    }
-  });*/
+            photos.push(photo);
+        }
+    }))
+};
