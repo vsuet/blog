c135c57c6b62614e4c90bef3fbb2fc290bd8df63
Hitok-Vandal <hitv@gmail.com>
2022-10-22 12:18:15 +0300

    test

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,8 @@
 const url = window.location.href;
 console.log(url);
-const head = document.getElementById("head");
-head.innerHTML = `
-    <title id="main-header">Заголовок</title>
+const body = document.getElementById("body");
+body.innerHTML = `
+    <p>${url}</p>
     
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <link rel="shortcut icon" href="image/favicon.ico" type="image/x-icon">
-    <link rel="stylesheet" href="styles/style.css">
+<script src="scripts/script.js"></script>
 `
